12942d11ee951bf7d6f106e7d2ba04e742dec266
kalaunidipak <kalaunidipak5@gmail.com>
2024-08-16 04:18:58 +0000

    feat: import database module in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import * as Joi from "joi";
+import { DatabaseModule } from "./database/database.module";
 
 @Module({
   imports: [
@@ -16,6 +17,17 @@ import * as Joi from "joi";
         POSTGRES_PASSWORD: Joi.string().required(),
         POSTGRES_DB: Joi.string().required(),
         PORT: Joi.number().port().default(4000),
+      }),
+    }),
+    DatabaseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        host: configService.get("POSTGRES_HOST"),
+        port: configService.get("POSTGRES_PORT"),
+        user: configService.get("POSTGRES_USER"),
+        password: configService.get("POSTGRES_PASSWORD"),
+        database: configService.get("POSTGRES_DB"),
       }),
     }),
   ],
