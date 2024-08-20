685717263776cf86efec9724f3d179a8c9d9c311
kalaunidipak <kalaunidipak5@gmail.com>
2024-08-20 08:16:02 +0000

    feat: remove sidebar and mobile nav, add notification and tabs

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,17 +1,15 @@
 import { Navbar } from "@web/components/dashboard/header/Navbar";
-import { Sidebar } from "@web/components/dashboard/sidebar/Sidebar";
+import { TabsMenu } from "@web/components/dashboard/tabs/TabsMenu";
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <Sidebar />
-      <div className="flex flex-col">
-        <Navbar />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          {children}
-        </main>
-      </div>
+    <div className="w-full min-h-screen flex flex-col">
+      <Navbar />
+      <main className="w-[80%] mx-auto">
+        <TabsMenu />
+        {children}
+      </main>
     </div>
   );
 }
--- apps/web/components/dashboard/header/Navbar.tsx
+++ apps/web/components/dashboard/header/Navbar.tsx
@@ -1,11 +1,18 @@
-import { MobileMenu } from "./MobileMenu";
 import { UserDropdown } from "./UserDropdown";
+import { Music } from "lucide-react";
+import { Notification } from "./Notification";
 
 export function Navbar() {
   return (
-    <header className="w-full sticky top-0 flex justify-between lg:justify-end h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <MobileMenu />
-      <UserDropdown />
+    <header className="w-full sticky top-0 flex justify-between h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav className="flex justify-center gap-x-4 items-center">
+        <Music />
+        <span className="font-bold">AMS</span>
+      </nav>
+      <div className="flex items-center gap-x-4">
+        <Notification />
+        <UserDropdown />
+      </div>
     </header>
   );
 }
