2e01b779250a770119563ffb4ae32c50125db43c
KaloniGiga <kalaunidipak5@gmail.com>
2024-08-24 23:24:59 +0545

    chore: update base query reauth file

--- apps/web/redux/base-query/base-query-with-reauth.ts
+++ apps/web/redux/base-query/base-query-with-reauth.ts
@@ -5,9 +5,7 @@ import {
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query";
 import { isFetchBaseQueryError } from "@web/lib/utils";
-import { Mutex } from "async-mutex";
 
-const mutex = new Mutex();
 export const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_SERVER_URL,
   credentials: "include",
@@ -18,36 +16,14 @@ export const baseQueryWithReauth = async (
   api: BaseQueryApi,
   extraOptions: object,
 ) => {
-  await mutex.waitForUnlock();
-
   let result = await baseQuery(args, api, extraOptions);
 
   if (result.error && isFetchBaseQueryError(result.error)) {
     if (result.error.status == 401) {
-      if (!mutex.isLocked()) {
-        const release = await mutex.acquire();
-
-        try {
-          const refreshResult = await baseQuery(
-            { credentials: "include", url: "v1/auth/refresh" },
-            api,
-            extraOptions,
-          );
-
-          if (refreshResult.data) {
-            result = await baseQuery(args, api, extraOptions);
-          } else {
-            window.location.href = "/";
-          }
-        } finally {
-          release();
-        }
-      } else {
-        await mutex.waitForUnlock();
-        result = await baseQuery(args, api, extraOptions);
-      }
+      window.location.href = "/";
+    } else {
+      result = await baseQuery(args, api, extraOptions);
     }
   }
-
   return result;
 };
